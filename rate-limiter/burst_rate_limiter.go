3ad1041f043d687fa7c1cb9d774f9c44703ac2de
Luciano Ferrari <luciano.ferrari@mercadolibre.com>
2020-12-13 14:37:48 -0300

    docs batch

--- rate-limiter/burst_rate_limiter.go
+++ rate-limiter/burst_rate_limiter.go
@@ -18,23 +18,23 @@ func main() {
 		fmt.Println("request", req, time.Now())
 	}
 
-	rafaga := make(chan time.Time, 3)
+	burst := make(chan time.Time, 3)
 
 	go func() {
 		for t := range time.Tick(1000 * time.Millisecond) {
 			for i := 1; i <= 3; i++ {
-				rafaga <- t
+				burst <- t
 			}
 		}
 	}()
 
-	rafagaRequest := make(chan int, 15)
+	burstRequest := make(chan int, 15)
 	for i := 1; i <= 15; i++ {
-		rafagaRequest <- i
+		burstRequest <- i
 	}
-	close(rafagaRequest)
-	for req := range rafagaRequest {
-		<-rafaga
+	close(burstRequest)
+	for req := range burstRequest {
+		<-burst
 		fmt.Println("request", req, time.Now())
 	}
 }
